aff2836f9c5b1326c557641911f3d61d6377941c
hassanimoueddenali <hassanimouedden@gmail.com>
2021-11-29 13:10:00 +0100

    update

--- prototype-A/index.js
+++ prototype-A/index.js
@@ -1,27 +1,21 @@
-// declaration des variables 
-var btn;
-var output;
-var number;
-var nombreDeviner;
-
+ // declaration des variables 
+ var randomNumber;
+ var userInput;
+ var message;
  // entrer: saiser 
- btn = document.getElementById('btn');
- output = document.getElementById('outputtext');
- number = Math.floor(Math.random() * 100);
-
+  randomNumber = Math.floor(Math.random() * 100);
+  userInput = prompt("Entrez un numéro");
  // traitement 
-btn.addEventListener('click', function(){
-  nombreDeviner = document.getElementById('userInput').value;
-  if (nombreDeviner == number){
-    output.innerHTML = 'Correct'
-  } else{
-    if (nombreDeviner < number){
-        output.innerHTML = "le nombre que vous avez choisir est plus petit"
-    }
-    else {
-      output.innerHTML = "le nombre que vous avez choisir est plus grand"
-    }
+ var n = parseInt(userInput);
+ if (randomNumber > userInput) {
 
-  } 
+     message = "il est supérieur à " + " " + userInput;
 
-});
+ }
+
+ else{
+
+     message = "il est plus petit que " + " " + userInput;
+ }
+ // sortie: Affichage
+ alert(message);
---
+++ prototype-B/index.js
@@ -0,0 +1,27 @@
+// declaration des variables 
+var btn;
+var output;
+var number;
+var nombreDeviner;
+
+ // entrer: saiser 
+ btn = document.getElementById('btn');
+ output = document.getElementById('outputtext');
+ number = Math.floor(Math.random() * 100);
+
+ // traitement 
+btn.addEventListener('click', function(){
+  nombreDeviner = document.getElementById('userInput').value;
+  if (nombreDeviner == number){
+    output.innerHTML = 'Correct'
+  } else{
+    if (nombreDeviner < number){
+        output.innerHTML = "le nombre que vous avez choisir est plus petit"
+    }
+    else {
+      output.innerHTML = "le nombre que vous avez choisir est plus grand"
+    }
+
+  } 
+
+});
